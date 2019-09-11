dfc7abd9d9fb705ed5ba8507edb0174836fe8d05
automagicallyio <max@automagically.io>
2019-09-11 19:59:57 -0400

    did the magic thing
    :)

--- Hands-on lab/lab-files/src/components/plan/Plans.js
+++ Hands-on lab/lab-files/src/components/plan/Plans.js
@@ -58,7 +58,8 @@ export default class Plans extends Component {
 
         return (
             <div class="container">
-                <Grid>
+<h3>Hello from Simplifying Devops session 2</h3>
+            <Grid>
                     <div class="plans">
                         <Row>{planCatalog}</Row>
                     </div>
