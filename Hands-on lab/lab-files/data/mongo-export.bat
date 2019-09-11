c070ab9c91dd1c25a81d359b11ff16083899bf2f
Max Yermakhanov <max@automagically.io>
2019-09-11 13:16:02 -0400

    added docker support

--- Hands-on lab/lab-files/data/mongo-export.bat
+++ Hands-on lab/lab-files/data/mongo-export.bat
@@ -1,5 +1,5 @@
 md c:\MongoExport
-cd c:\Program Files\MongoDB\Server\3.6\bin
+cd c:\Program Files\MongoDB\Server\4.2\bin
 mongoexport --db best-for-you-organics --collection plans --out c:\MongoExport\plans.json
 mongoexport --db best-for-you-organics --collection users --out c:\MongoExport\users.json
 mongoexport --db best-for-you-organics --collection orders --out c:\MongoExport\orders.json
